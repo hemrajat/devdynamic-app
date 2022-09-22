7be10dacdc0af4d5004e0d1bb2e336a674c2b466
Hemraj Choudhary <85800858+hemrajat@users.noreply.github.com>
2022-09-22 09:14:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,8 @@ import './App.css'
 import Dashboard from './Components/Dashboard'
 import Sidebar from './Components/Sidebar'
 const App = () => {
-  const stageCanvasRef = useRef(null)
-
-  // useEffect will run on stageCanvasRef value assignment
-  useEffect(() => {
-    // The 'current' property contains info of the reference:
-    // align, title, ... , width, height, etc.
-    if (stageCanvasRef.current) {
-      let height = stageCanvasRef.current.offsetHeight
-      let width = stageCanvasRef.current.offsetWidth
-      console.log(height, width)
-    }
-  }, [stageCanvasRef])
   return (
-    <div className='app' ref={stageCanvasRef}>
+    <div className='app'>
       <Sidebar />
       <Dashboard />
     </div>
